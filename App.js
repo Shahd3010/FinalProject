5ef6bbe1ef664e9b34e9d1c215737f3ffe0947e4
Shahd3010 <73404827+Shahd3010@users.noreply.github.com>
2023-04-16 21:38:28 +0300

    updates

--- App.js
+++ App.js
@@ -1,36 +1,37 @@
-import React, { useState, createContext, useContext, useEffect } from "react";
+import React from "react";
 //import { AppRegistry } from "react-native";
 //import App from "./App";
 //import { FinalProject as appName } from "./app.config.js";
 
 //AppRegistry.registerComponent(appName, () => App);
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { WorkScreen } from "./src/features/works/screens/works.screen";
-import { ThemeProvider } from "styled-components/native";
-import { Text, View, ActivityIndicator } from "react-native";
+//import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+//import { WorkScreen } from "./src/features/works/screens/works.screen";
+//import { ThemeProvider } from "styled-components/native";
+//import { Text, View, ActivityIndicator } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 //import { onAuthStateChanged } from "firebase/auth";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { SafeArea } from "./src/components/utility/safe-area.component";
-import Chat from "./src/features/works/screens/Chat";
-import { SettingsScreen } from "./src/features/works/screens/setting.screen";
+//import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+//import { SafeArea } from "./src/components/utility/safe-area.component";
+//import Chat from "./src/features/works/screens/Chat";
+//import { SettingsScreen } from "./src/features/works/screens/setting.screen";
 import Login from "./src/features/works/screens/Login";
 import HomeScreen from "./src/features/works/screens/HomeScreen";
 import Signup from "./src/features/works/screens/Signup";
 import SignUpType from "./src/features/works/screens/SignUpType";
-import {
+/*import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
 import { theme } from "./src/infrastructure/theme";
 import { MaterialIcons } from "@expo/vector-icons";
-import WorkerProfile from "./src/features/works/screens/WorkerProfile";
+import WorkerProfile from "./src/features/works/screens/WorkerProfile";*/
 import SignUpWorker from "./src/features/works/screens/SignUpWorker";
 //import auth from "./config/firebase";
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator(); /*
 function RootNavigator() {
   return (
     <NavigationContainer>
@@ -39,13 +40,13 @@ function RootNavigator() {
   );
 }
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();*/
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="SignUpType" component={SignUpType} />
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SignUpType" component={SignUpType} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
         <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="SignUpWorker" component={SignUpWorker} />
